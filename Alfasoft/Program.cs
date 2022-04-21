0dff6c4b10c279b589aa37fc8c4dbce20fda2a6a
Juvenal Viana <junior.176@outlook.com>
2022-04-21 13:33:45 -0300

    feat: Adicionado arquivo de log

--- Alfasoft/Program.cs
+++ Alfasoft/Program.cs
@@ -10,13 +10,29 @@ namespace Alfasoft
 {
     class Program
     {
-        static void Main(string[] args2)
+        static int intervalRequest = 5000;
+        static string logFile = "log.txt";
+        static void Main(string[] args)
         {
-            string[] args = {"c:\\usuarios.txt"}; //debug only
+            //string[] args = {"c:\\usuarios.txt"}; //debug only
 
-            List<string> users = new List<String> {};
-            if (args.Length == 0) exitApplication();
-            if (!File.Exists(args[0])) exitApplication();
+            List<string> users = new List<string>{};
+            
+            if (args.Length == 0) exitApplication("Parameter is mandatory.");
+
+            if (File.Exists(logFile)) {
+
+                FileInfo log_info = new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "\\"+logFile);
+
+                TimeSpan dateDiff = DateTime.Now - log_info.LastWriteTime;
+
+                if (dateDiff.TotalSeconds < 60) {
+                    exitApplication("Wait "+ (60 - dateDiff.TotalSeconds).ToString("0") + " seconds and run again.");
+                }
+
+            }
+
+            if (!File.Exists(args[0])) exitApplication("File not found.");
             else {
                 
                 foreach (string line in File.ReadLines(args[0]))
@@ -28,7 +44,8 @@ namespace Alfasoft
 
             foreach (string user in users) {
 
-                var requisicaoWeb = WebRequest.CreateHttp("https://api.bitbucket.org/2.0/users/"+user);
+                string url = "https://api.bitbucket.org/2.0/users/" + user;
+                var requisicaoWeb = WebRequest.CreateHttp(url);
                 requisicaoWeb.Method = "GET";
                 requisicaoWeb.ContentType = "application/x-www-form-urlencoded";
                 requisicaoWeb.UserAgent = "JuvenalViana";
@@ -38,13 +55,12 @@ namespace Alfasoft
                     using (var resposta = requisicaoWeb.GetResponse())
                     {
                         var streamDados = resposta.GetResponseStream();
-                        StreamReader reader1 = new StreamReader(streamDados);
-                        object objResponse = reader1.ReadToEnd();
-
+                        StreamReader reader = new StreamReader(streamDados);
+                        object objResponse = reader.ReadToEnd();
+                        WriteLog("User " + user + ": " + objResponse.ToString());
                         User result = JsonConvert.DeserializeObject<User>(objResponse.ToString());
-
-                        Console.WriteLine(objResponse.ToString());
-                        Thread.Sleep(5000);
+                        Console.WriteLine("User " + user + ", name: " + result.display_name + ", url: " + url + ", output: "+ objResponse.ToString());
+                        Thread.Sleep(intervalRequest);
                         streamDados.Close();
                         resposta.Close();
 
@@ -52,18 +68,26 @@ namespace Alfasoft
                 }
                 catch (WebException we) {
                     HttpWebResponse response = (HttpWebResponse)we.Response;
+                    WriteLog("User " + user + " not found.");
                     Console.WriteLine("User "+user+" not found.");
-                    Thread.Sleep(5000);
+                    Thread.Sleep(intervalRequest);
                 }
 
             }            
 
         }
 
-        static void exitApplication() {
-            Console.WriteLine("Arquivo de configuração inválido.");
-            Thread.Sleep(5000);
+        static void exitApplication(string msg) {
+            Console.WriteLine(msg);
+            Thread.Sleep(intervalRequest);
             Environment.Exit(-1);
+        }
+        static void WriteLog(string log) {
+            string docPath = AppDomain.CurrentDomain.BaseDirectory;
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, logFile), true))
+            {
+                outputFile.WriteLine(log);
+            }
         }
     }
 }
